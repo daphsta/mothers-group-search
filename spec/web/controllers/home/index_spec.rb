33208ff0562648198f1b6834f4e03b0d65ec2a32
Daphne Chan <yiingshan@gmail.com>
2018-05-17 16:13:23 +1000

    Remove unnecessary view test

--- spec/web/controllers/home/index_spec.rb
+++ spec/web/controllers/home/index_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../../../spec_helper'
+require_relative '../../../../spec_helper'
 
 describe Web::Controllers::Home::Index do
   let(:action) { Web::Controllers::Home::Index.new }
